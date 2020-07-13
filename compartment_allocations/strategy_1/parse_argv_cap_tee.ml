9adbc933268f391012cabe606093a27087cce883
Sai Venkata Krishnan <saiganesha5.svkv@gmail.com>
2020-07-13 18:28:36 +0530

    Update compartment allocations

--- compartment_allocations/strategy_1/parse_argv_cap_tee.ml
+++ compartment_allocations/strategy_1/parse_argv_cap_tee.ml
@@ -1,9 +1,9 @@
 (*
-camlParse_argv__parse:231
-camlParse_argv__skip_white:231
-camlParse_argv__split:231
-camlParse_argv__inner:231
-camlParse_argv__is_data:231
+camlParse_argv__parse:254
+camlParse_argv__skip_white:254
+camlParse_argv__split:254
+camlParse_argv__inner:254
+camlParse_argv__is_data:254
 camlParse_argv_:255
-camlParse_argv__fun:231
+camlParse_argv__fun:254
 *)
